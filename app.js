59512ec1e97d2e09cc3f6751978ec9d20e698933
brenley-woodard <brenleyew@gmail.com>
2022-11-01 11:23:42 -0700

    button clicks are correctly revealing gary

--- app.js
+++ app.js
@@ -1,13 +1,52 @@
-/* Imports */
+/* Get DOM Elements */
 const patrickButton = document.getElementById('patrick-button');
 const squidwardButton = document.getElementById('squidward-button');
 const spongebobButton = document.getElementById('spongebob-button');
 
-/* Get DOM Elements */
+const patrickImg = document.getElementById('patrick-img');
+const squidwardImg = document.getElementById('squidward-img');
+const spongebobImg = document.getElementById('spongebob-img');
 
 /* State */
 
 /* Events */
+patrickButton.addEventListener('click', () => {
+    const randomLocation = Math.ceil(Math.random() * 3);
+    if (randomLocation === 1) {
+        //add to wins
+        patrickImg.classList.add('reveal');
+    } else if (randomLocation === 2) {
+        squidwardImg.classList.add('reveal');
+    } else {
+        spongebobImg.classList.add('reveal');
+    }
+});
+
+squidwardButton.addEventListener('click', () => {
+    const randomLocation = Math.ceil(Math.random() * 3);
+    console.log(randomLocation);
+    if (randomLocation === 1) {
+        patrickImg.classList.add('reveal');
+    } else if (randomLocation === 2) {
+        squidwardImg.classList.add('reveal');
+        //add wins
+    } else {
+        spongebobImg.classList.add('reveal');
+    }
+});
+
+spongebobButton.addEventListener('click', () => {
+    const randomLocation = Math.ceil(Math.random() * 3);
+    console.log(randomLocation);
+    if (randomLocation === 1) {
+        patrickImg.classList.add('reveal');
+    } else if (randomLocation === 2) {
+        squidwardImg.classList.add('reveal');
+    } else {
+        spongebobImg.classList.add('reveal');
+        //add wins
+    }
+});
 
 /* Display Functions */
 
